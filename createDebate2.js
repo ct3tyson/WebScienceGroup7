2ced35fac65c36018b13690d110faf4a87431a75
ct3tyson <tysonc@rpi.edu>
2017-04-26 14:37:10 -0400

    Update createDebate2.js

--- createDebate2.js
+++ createDebate2.js
@@ -1,29 +1,77 @@
+/*var socket=io();
+
+
+$("#debate").click(function() {
+  var debateTopic = $('#debateTopic').val();
+  var debateBio = $('#newDebate').val();
+  
+  
+  //makes sure fields aren't empty cause if it is
+  //well it wouldn't/shouldn't work
+  if (debateTopic == '' || debateBio == '' ) {
+    document.getElementById("error").innerHTML = "Please fill out all fields.";
+  }else {
+    
+    var user_data = {
+      'topic': debateTopic,
+      'debate': debateBio,
+    };
+    //check if user data is correct
+    socket.emit('user_debate_data', user_debate_data);
+    
+    socket.on('debateCheck', function(check){
+      if (!check) {
+        document.getElementById("error").innerHTML = 'Debate is already being talked about!';
+      }else {
+        document.getElementById("error").innerHTML = 'Debate Created!';
+
+          }
+        });
+        //redirect to different page
+       
+      }
+    });
+    */
+
+//<textarea type="textarea" id="newDebate" data-min-length="10" ng-required placeholder="What's your debate? If you cant think of anything... So about chicken tenders and chicken nuggets..."></textarea>
+
 var socket=io();
 
 
 angular.module('app',[]).controller('debateController', function($scope, $http) {
-	  $scope.user = '';
-	  var getmdblist = function() {
-		socket.emit('getList', $scope.user);
-	}
-	//get user logged in
+    $scope.user = '';
+    var getmdblist = function() {
+    socket.emit('getList', $scope.user);
+  }
+  //get user logged in
     $http({
       method: 'GET',
       url: 'http://localhost:3000/user'
     }).then(function successCallback(response) {
       $scope.user = response.data;
-	  getmdblist();
+    getmdblist();
     }, function errorCallback(response) {
       console.log(response);
     });
-
-    $scope.createDebate = function() {
-  var debate = {
+  
+  
+//add item to user bucketlist
+$scope.addDebate = function() {
+  var item = {
     topic: document.getElementById("debateTopic").value,
-    data: document.getElementById("newDebate").value,
+    debate: document.getElementById("newDebate").value,
     user: $scope.user,
-    time: new Date(),
+    time: new Date()
   }
-  socket.emit('createDebate',debate);
+  socket.emit('addDebate',item);
   getmdblist();
 }
+
+socket.on('receiveList',function(list){
+  $scope.$apply(function() {
+    $scope.items = list;
+  });
+ 
+})
+ 
+});
